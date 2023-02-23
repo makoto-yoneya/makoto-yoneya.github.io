4f83c53855c956b92bba82f283dd87c34ccbbda9
Makoto Yoneya <makoto-yoneya@outlook.jp>
2023-02-23 17:41:05 +0900

    update file

--- LAMMPS-organics/install_WSLmisc.sh
+++ LAMMPS-organics/install_WSLmisc.sh
@@ -51,28 +51,28 @@ else
 fi
 
 if [ ! -e  $HOME/.bash_aliases ]; then
-	echo "# created by install_ac.sh" > .bash_aliases
+	echo "# created by install_WSLmisc.sh" > $HOME/.bash_aliases
 fi	
 
 grep MDonWSL $HOME/.bash_aliases > /dev/null
 if [ $? -eq 1 ]; then
 	echo "* updating .bash_aliases"
-	echo "# MDonWSL" >> .bash_aliases
-	echo "set -o notify" >> .bash_aliases
+	echo "# MDonWSL" >> $HOME/.bash_aliases
+	echo "set -o notify" >> $HOME/.bash_aliases
 	if [ -d $HOME/bin ]; then
-		echo "export PATH=\$HOME/bin:\$PATH" >> .bash_aliases
+		echo "export PATH=\$HOME/bin:\$PATH" >> $HOME/.bash_aliases
 	fi
 	if [ -x $HOME/opt/amber20 ]; then
-		echo "export AMBERHOME=\$HOME/opt/amber20" >> .bash_aliases
-		echo "export PATH=\$AMBERHOME/bin:\$PATH" >> .bash_aliases
+		echo "export AMBERHOME=\$HOME/opt/amber20" >> $HOME/.bash_aliases
+		echo "export PATH=\$AMBERHOME/bin:\$PATH" >> $HOME/.bash_aliases
 	fi
 	if [ -x $HOME/opt/moltemplate ]; then
-		echo "export MTHOME=\$HOME/opt/moltemplate/moltemplate" >> .bash_aliases
-		echo "export PATH=\$MTHOME/scripts:\$PATH" >> .bash_aliases
+		echo "export MTHOME=\$HOME/opt/moltemplate/moltemplate" >> $HOME/.bash_aliases
+		echo "export PATH=\$MTHOME/scripts:\$PATH" >> $HOME/.bash_aliases
 	fi
 	VMDDIR="/mnt/c/Program Files/VMD"
 	if [ -x "$VMDDIR" ]; then
-		echo "export VMDDIR=\"/mnt/c/Program Files/VMD\"" >> .bash_aliases
-		echo "export PATH=\$VMDDIR:\$PATH" >> .bash_aliases
+		echo "export VMDDIR=\"/mnt/c/Program Files/VMD\"" >> $HOME/.bash_aliases
+		echo "export PATH=\$VMDDIR:\$PATH" >> $HOME/.bash_aliases
 	fi
 fi
